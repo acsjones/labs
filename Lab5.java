1a3c28227666abccbe7dcde15edcc1ef1caeaca1
Ashlei Jones <acsjones06@gmail.com>
2016-08-25 16:32:07 -0400

    Initial commit

--- Lab5.java
+++ Lab5.java
@@ -15,13 +15,17 @@ public class Lab5 {
 		while (choice.equalsIgnoreCase("y")) {
 			System.out.println("How many sides should each die have?");
 			int number = sc.nextInt();
-
+			int answer = roll(number);
+			int answer2 = roll(number);
 			System.out.println("Roll results:");
-			System.out.println(cube(number));
-			System.out.println(cube(number));
-			
-			if ((cube(number) ==1)) {
+			System.out.println(answer);
+			System.out.println(answer2);
+
+			if (answer == 1 && answer2 == 1) {
 				System.out.println("Lucky! Snake eyes!");
+			}
+			if (answer == 6 && answer2 == 6) {
+				System.out.println("Wow! Box Car!");
 			}
 
 			System.out.println("Try again?");
@@ -31,7 +35,7 @@ public class Lab5 {
 
 	}
 
-	public static int cube(int number) {
+	public static int roll(int number) {
 		return (int) (Math.random() * number) + 1;
 
 	}
