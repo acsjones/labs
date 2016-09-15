1b65163ee874e486976a685b17e0a9229452b41a
Ashlei Jones <acsjones06@gmail.com>
2016-09-15 09:25:11 -0400

    Initial Commit

--- battingApp/BatterTest.java
+++ battingApp/BatterTest.java
@@ -7,10 +7,15 @@ import org.junit.Test;
 public class BatterTest {
 	
 	@Test
-	public void whenBaseEarnedIs0_1ExpectedBattingAverageIs_500() {
+	public void whenBaseEarnedIs0_1ExpectedSlugPerIs_500() {
 	Batter b = new Batter(null);
-	assertEquals("0.500",b.getFormattedBatAvg());
+	assertEquals("0.500",b.getFormattedSlugPer());
 	}
+	@Test
+	public void whenBaseEarnedIs0_1_2ExpectedSlugPerIs_1() {
+		Batter b = new Batter(null);
+		assertEquals("1.000",b.getFormattedSlugPer());
+		}
 	
 	
 	
